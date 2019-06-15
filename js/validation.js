f6df310d09f11a97a209a934b9646cff3674bf51
[trapintrovert] <[mcneo062@gmail.com]>
2019-06-15 16:13:27 +0100

    modifications

--- js/validation.js
+++ js/validation.js
@@ -1,13 +1,11 @@
 const name = document.getElementById('name');
 const email = document.getElementById('email');
-const message = document.getElementById('message');
 
 name.addEventListener('blur', valName);
 email.addEventListener('blur', valEmail);
-message.addEventListener('blur', valMessage);
 
 function valName() {
-  const re = /^[a-zA-Z]{2,20}$/;
+  const re = /^[a-zA-Z\s]{2,20}$/;
   if(!re.test(name.value)) {
     name.classList.add('is-invalid');
   } else {
@@ -21,14 +19,5 @@ function valEmail() {
     email.classList.add('is-invalid');
   } else {
     email.classList.remove('is-invalid');
-  }
-}
-
-function valMessage() {
-  const re = /^[a-zA-Z]$/;
-  if(!re.test(message.value)) {
-    message.classList.add('is-invalid');
-  } else {
-    message.classList.remove('is-invalid');
   }
 }
